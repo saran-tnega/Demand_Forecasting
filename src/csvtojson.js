eda7f6546daceddb89258185601a11b49ce49efd
saran-tnega <124550267+saran-tnega@users.noreply.github.com>
2023-03-22 23:19:55 +0530

    created charts
    based on the current time visualize what will be the demand for the next 15 minutes and we can also able to know the no of demand upto 2024 march.

---
+++ src/csvtojson.js
@@ -0,0 +1,33 @@
+// const csv = require('csvtojson');
+
+// csv()
+// //   .fromFile('path/to/your/data.csv')
+//         .fromFile("./data24.csv")
+// .then((json) => {
+//     const data = json.map((row) => {
+//       return {
+//         datetime: row.date + row.time,
+//         demand: parseInt(row.demand),
+//       };
+//     });
+
+//     console.log(data);
+//   });
+
+// // const csv = require('csvtojson');
+
+// //         csv()
+// //         .fromFile("./data24.csv")
+// //         .then((json) => {
+// //             const data = json.map((row) => {
+// //             return {
+// //         /* Creating a date string in the format `YYYY-MM-DDTHH:MM:SSZ` */
+// //                 datetime: row.date + row.time,
+// //                 demand: parseInt(row.demand),
+// //             };
+// //             });
+
+// //             console.log(data);
+// //         });
+
+//         // module.exports = data;
